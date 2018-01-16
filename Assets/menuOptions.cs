53112ff5ae0fa53f945fa2e960226978fbeb0962
Michael Kaplor <s179059@students.aie.edu.au>
2018-01-16 12:53:22 -0800

    fresh commit for shea #1

---
+++ Assets/menuOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuOptions : MonoBehaviour
+{
+    public GameObject startMarker;
+    public GameObject endMarker;
+    public GameObject camera;
+    public void optionsClicked()
+    {
+
+        camera.transform.position = Vector3.Lerp(startMarker.transform.position, endMarker.transform.position, 10);
+    }
+
+}
